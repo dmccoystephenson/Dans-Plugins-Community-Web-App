0a9e8b6a89d77e06f97f5a75bdcaf950eedff9ee
dmccoystephenson <dmccoystephenson@gmail.com>
2022-02-01 20:01:13 -0700

    Repurposed the project for the Dan's Plugins community.

--- src/main/java/dansplugins/site/DPCommunityApp.java
+++ src/main/java/dansplugins/site/DPCommunityApp.java
@@ -1,15 +1,15 @@
 
-package com.example.demo;
+package dansplugins.site;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DPCommunityApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DPCommunityApp.class, args);
 	}
 
 	@RequestMapping("/")
--- src/test/java/dansplugins/site/DPCommunityAppTests.java
+++ src/test/java/dansplugins/site/DPCommunityAppTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package dansplugins.site;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DPCommunityAppTests {
 
 	@Test
 	void contextLoads() {
